428325677339f2ccdb62950c9250cd7826b89348
apayah90 <apayah90@gmail.com>
2019-05-08 12:41:55 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,26 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'pujiyulitomowebappserver.database.windows.net');
-define('DB_USERNAME', 'apayah90');
-define('DB_PASSWORD', 'terserah90!');
-define('DB_NAME', 'pujiyulitomowebapp');
+$host = 'pujiyulitomowebappserver.database.windows.net';
+$username = 'apayah90';
+$password = 'terserah90!';
+$db_name = 'pujiyulitomowebapp';
  
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
  
+ //Establishes the connection
+$link = mysqli_init();
+mysqli_real_connect($link, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($link)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+//
+
+
 ?>
